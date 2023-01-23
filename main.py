399417bb16ab0fb5efa6c856ffec1f2494c630d2
crutoboy <vanyalex0192837465@gmail.com>
2023-01-23 21:02:17 +0300

    v2.0

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import os
 import json
 
@@ -6,7 +8,7 @@ from telebot import types
 
 
 config = json.load(open('config.json', encoding="utf-8"))
-token, root_path = config['token'], config['path_with_books']
+token, src_path, dst_path, root_path, admin_id, interval = config['token'], config['src_path'], config['dst_path'], config['root_path'], config['admin'], config['interval_in_minuts']
 bot = telebot.TeleBot(token)
 dict_with_active_catalogs = {}
 
@@ -15,11 +17,11 @@ os.chdir(root_path)
 
 def create_markup (path: str) -> types.ReplyKeyboardMarkup:
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    files = os.listdir(path)
+    files = os.listdir(f'{dst_path}/{path}')
 
     if files:
         for i in files:
-            if os.path.isfile(f'{path}/{i}'):
+            if os.path.isfile(f'{dst_path}/{path}/{i}'):
                 markup.add(types.KeyboardButton(f'🔈 {i}'))
             else:
                 markup.add(types.KeyboardButton(i))
@@ -37,6 +39,7 @@ def start(message):
     bot.send_message(message.from_user.id,
 '''Это бот для прослушывания аудио-книг
 Ниже указан каталог книг, при внезапном его пропадании введите команду /start или /catalog''')
+    catalog(message)
 
 
 @bot.message_handler(commands=['catalog'])
@@ -49,7 +52,10 @@ def catalog(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    path = dict_with_active_catalogs[message.from_user.id]
+    try:
+        path = dict_with_active_catalogs[message.from_user.id]
+    except:
+        path = '/'
     if message.text[0] == '↩':
         dict_with_active_catalogs.update([[message.from_user.id, '/'.join(path.split('/')[:-1])]])
         bot.send_message(message.from_user.id,
@@ -60,13 +66,44 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id,
         message.text,
         reply_markup=create_markup(dict_with_active_catalogs[message.from_user.id]))
-    elif message.text[0] == '🔈' and os.path.isfile(f'{path}/{message.text[2:]}'):
-        bot.send_audio(message.from_user.id, open(f'{path}/{message.text[2:]}', 'rb'))
-    elif os.path.isdir(f'{path}/{message.text}'):
+    elif message.text[0] == '🔈' and os.path.isfile(f'{dst_path}/{path}/{message.text[2:]}'):
+        bot.send_audio(message.from_user.id, open(f'{dst_path}/{path}/{message.text[2:]}', 'r').read())
+    elif os.path.isdir(f'{dst_path}/{path}/{message.text}'):
         dict_with_active_catalogs.update([[message.from_user.id, f'{path}/{message.text}']])
         bot.send_message(message.from_user.id,
         message.text,
         reply_markup=create_markup(dict_with_active_catalogs[message.from_user.id]))
+
+
+def start_interval_action (foo, interval_in_min: int) -> None:
+    while True:
+        foo()
+        time.sleep(interval_in_min * 60)
+
+def recursion_check_files(path: str = '') -> None:
+    cur_src = f'{src_path}/{path}'
+    cur_dst = f'{dst_path}/{path}'
+    content_src = os.listdir(cur_src)
+
+    if not os.path.isdir(cur_dst):
+        os.mkdir(cur_dst)
+
+    for i in content_src:
+        if (os.path.isfile(f'{cur_src}/{i}')) and ((not os.path.isfile(f'{cur_dst}/{i}')) or (os.path.getmtime(f'{cur_src}/{i}') > os.path.getmtime(f'{cur_dst}/{i}'))):
+            with open(f'{cur_dst}/{i}', 'w') as file:
+                doc = bot.send_document(admin_id, open(f'{cur_src}/{i}', 'rb'))
+                file.write(doc.audio.file_id)
+        elif os.path.isdir(f'{cur_src}/{i}'):
+            recursion_check_files(f'{path}/{i}')
+
+    content_dst = os.listdir(cur_dst)
+    for i in content_src:
+        content_dst.remove(i)
+    
+    for i in content_dst:
+        os.remove(f'{cur_dst}/{i}')
+
+threading.Thread(target=start_interval_action, args=(recursion_check_files, interval)).start()
 
 
 if __name__ == '__main__':
